4cfe8fa08d62444a8b1b4f109871895a99a50e0f
Nikita Budiakov <budyakov91@gmail.com>
2024-10-02 22:33:23 -0400

    implemeted store, reducers and action

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // App.js
 import React from 'react';
+import './App.css';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
-import './App.css'
+import SuperCoins from './Components/SuperCoin';
 const App = () => {
   return (
  
@@ -10,6 +11,7 @@ const App = () => {
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
+        <SuperCoins />
       </div>
 
   );
